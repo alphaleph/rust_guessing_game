d87ddea2f1d756113bb7e43a9f2b5cfa1fbaf4b7
Alpha Chau <chaua0927@gmail.com>
2019-10-22 14:05:19 -0400

    Initial commit, First guess

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+
+fn main() {
+    println!("Welcome to the Rust Guessing Game.");
+    println!("Please enter a guess: ");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
