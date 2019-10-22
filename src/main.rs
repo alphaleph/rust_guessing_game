fff99e8a6335908eb3f012ebaf9d206ffebaf2fd
Alpha Chau <chaua0927@gmail.com>
2019-10-22 14:35:50 -0400

    Added initial matching

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -15,6 +16,15 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: i32 = guess.trim().parse()
+        .expect("Please type an integer!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Try higher."),
+        Ordering::Greater => println!("Try lower."),
+        Ordering::Equal => println!("You got it!")
+    }
 
 }
